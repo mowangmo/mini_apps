4d668dd1c87c1fac0b03795cf53f7e35534f621d
wangmo <wangmo@syberos.com>
2017-11-02 18:07:59 +0800

    4 day

--- school_manager/src/models.py
+++ school_manager/src/models.py
@@ -6,4 +6,16 @@ import os
 from conf import settings
 from src import unique_id
 
+class Base_Model:   #父类
+    def dump(self):     #将对象dump到文件
+        file_path = os.path.join(self.db_path,str(self.uuid))     #设置dump的路径，id为文件名
+        pickle.dump(self,open(file_path,'wb'))  #wb 以二进制写模式打开
+
+    @classmethod    #类方法,无需对象，可通过类直接调用
+    def all_obj_list(cls):
+        all_obj_l = []
+        for file_name in os.listdir(cls.db_path):   #将对象都加入到列表
+            file_path = os.path.join(cls.db_path,file_name)
+            all_obj_l.append(pickle.load(open(file_path,'rb')))     #rb  以二进制读模式打开
+        return all_obj_l
 
--- school_manager/test.py
+++ school_manager/test.py
@@ -1,15 +1,54 @@
-class A:
-    def hahaha(self):
-        print('A')
+# IND = 'ON'
+# def checkind():
+#     return (IND == 'ON')
+# class Kls(object):
+#      def __init__(self,data):
+#         self.data = data
+# def do_reset(self):
+#     if checkind():
+#         print('Reset done for:', self.data)
+# def set_db(self):
+#     if checkind():
+#         self.db = 'new db connection'
+#         print('DB connection made for:',self.data)
+# ik1 = Kls(12)
+# do_reset(ik1)
+# set_db(ik1)
 
-class B(A):
-    def hahaha(self):
-        super().hahaha()
-        super(B,self).hahaha()
-        A.hahaha(self)
-        print('B')
 
-a = A()
-b = B()
-b.hahaha()
-super(B,b).hahaha()
+# IND = 'no'
+# class Kls(object):
+#     def __init__(self, data):
+#         self.data = data
+#     @staticmethod
+#     def checkind():
+#         return (IND == 'ON')
+#     def do_reset(self):
+#         if self.checkind():
+#             print('Reset done for:', self.data)
+#     def set_db(self):
+#         if self.checkind():
+#             self.db = 'New db connection'
+#         print('DB connection made for: ', self.data)
+# ik1 = Kls(12)
+# ik1.do_reset()
+# ik1.set_db()
+
+class Kls(object):
+    def __init__(self, data):
+        self.data = data
+    def printd(self):
+        print(self.data)
+    @staticmethod
+    def smethod(*arg):
+        print('Static:', arg)
+    @classmethod
+    def cmethod(*arg):
+        print('Class:', arg)
+
+ik = Kls(23)
+ik.printd()
+ik.smethod()
+ik.cmethod()
+Kls.smethod()
+Kls.cmethod()
