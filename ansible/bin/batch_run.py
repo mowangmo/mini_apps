cf72d70d5c3cb3272b91eef7d076206927128d45
mo <69710083@qq.com>
2017-11-16 07:51:52 +0800

    3 n

--- ansible/bin/batch_run.py
+++ ansible/bin/batch_run.py
@@ -1,4 +1,11 @@
+import os,sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
 from src import run
+from lib import my_optparse
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
 
 if __name__ == '__main__':
-    run.cmd()
+    my_optparse.my_opt()
